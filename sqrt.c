b04ed4339bf54802c4dfa8f7e86b759ccd0abdb3
thisconnect <p7tryk@gmail.com>
2019-10-10 13:01:39 +0200

    sqrt implementation

---
+++ sqrt.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+double positive(double input)
+{
+  if(input<0)
+    return -(input);
+  return input;
+      
+}
+double check(double w, double x)
+{
+  double output = positive(w*w-x);
+  if(output > 1)
+    return 1;
+  return 0;
+}
+
+double newsqrt(double input)
+{
+  double output = 1;
+
+  for(output; check(output,input);output=0.5*(input/output+output))
+    {
+      int i;
+    }
+
+  return output;
+}
+
+int main()
+{
+  double i = 0;
+
+  scanf("%lf",&i);
+
+  double x = i;
+  
+  
+  i=newsqrt(i);
+  x=sqrt(x);
+  printf("moj %lf,\nlib %lf,\nerr %lf\n", i, x, (double) i-x);
+  
+  
+
+}
---
+++ zajecia0.md
@@ -0,0 +1,6 @@
+dydmat.mimuw.edu.pl
+milewski s skrypty numeryczne
+wazniak.mimuw.edu.pl
+
+sumator 4bitow
+dodawanie przez sumowanie
